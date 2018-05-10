9b33c4ae6daebecd516adacc19feab765bc3a86e
Timothy J O'Connell <toconnell@tyrannybelle.com>
2018-05-10 10:46:37 -0400

    abstrated data to json file. created html templates via ng-include

--- app.js
+++ app.js
@@ -6,38 +6,34 @@ app.filter('trustedHTML',
    }
 );
 
-app.controller('bodyController', function($scope) {
+app.controller('bodyController', function($scope, $http) {
+
+	//
+	// init
+	//
 
     $scope.scratch = {
         activeTab: 'infoTab', 
     };
+
+	$http.get('data.json').then(
+	    function(payload) {
+     		$scope.tabs = payload.data.tabs;
+        },
+        function(errorPayload) {
+            console.error("Could not load application data!" + errorPayload);
+        }
+	);
+
+
+	//
+	// methods
+	//
 
     $scope.setActiveTab = function(tabId) {
         $scope.scratch.activeTab = tabId;
     };
 
 
-    $scope.tabs = [
-        {   'id': 'infoTab',
-            'title': 'Information',
-            'subtitle': 'Schedule',
-            'body': 'Coming Soon!',
-        },
-        {
-            'id': 'venuesTab',
-            'title': 'Venues',
-            'body': '<h3>Directions</h3><h3>Info</h3><h3>Maps</h3>',
-        },
-        {
-            'id': 'hotelsTab',
-            'title': 'Hotels',
-            'body': 'Coming Soon!'
-        },
-        {
-            'id': 'registryTab',
-            'title': 'Registry',
-            'body': 'Registry on <a href="https://www.amazon.com/wedding/charlotte-barnes-tim-oconnell-chicago-august-2018/registry/1CBVGHB6NKRIP">Amazon</a>',
-        },
-    ];
 
 });
